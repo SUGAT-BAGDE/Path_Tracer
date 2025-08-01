f83710814559cc467735d89596a2e553f7e8c6ff
Sugat Bagde <bagdesugat.2006@gmail.com>
2025-08-01 20:11:15 +0530

    Added a Camera Class

--- src/ray_tracer/mod.rs
+++ src/ray_tracer/mod.rs
@@ -1,3 +1,6 @@
 pub mod camera;
 pub mod renderer;
 pub(crate) mod utils;
+pub mod ray;
+
+use ray::Ray;
--- src/ray_tracer/renderer.rs
+++ src/ray_tracer/renderer.rs
@@ -2,14 +2,17 @@ use std::default::Default;
 
 use std::time::{Duration, Instant};
 
-use glam::{Vec2, Vec3, Vec4};
+use glam::{Vec3, Vec4};
 
+use crate::ray_tracer::camera::Camera;
+use crate::ray_tracer::Ray;
 use crate::ray_tracer::utils::convert_to_argb;
 
 pub struct RayTracer {
     width: u32,
     height: u32,
     pixels: Vec<u32>,
+    camera: Camera,
     last_render_time: Duration,
 }
 
@@ -20,6 +23,13 @@ impl RayTracer {
             height: 0,
             pixels: vec![],
             last_render_time: Duration::from_secs(0),
+            camera : Camera::new(
+                Vec3::ZERO, 
+                Vec3::ZERO,
+                35.0,
+                55.0,
+                [0,0]
+            )
         }
     }
 
@@ -37,31 +47,37 @@ impl RayTracer {
         }
     }
 
+    fn init_scene(&mut self) {
+        self.camera.position = Vec3::new(0.0, 0.0, 2.0);
+    }
+
     fn set_size(&mut self, size: [u32; 2]) {
         self.width = size[0];
         self.height = size[1];
+        self.camera.set_image_resolutions(size);
     }
 
     pub fn render(&mut self, width: u32, height: u32) {
+        self.init_scene();
+
         let render_start_time = Instant::now();
 
         self.set_size([width, height]);
 
         self.pixels = vec![0xFFFFFFFF_u32; (width * height) as usize];
-        let aspect_ratio = width as f32 / height as f32;
+        // let aspect_ratio = width as f32 / height as f32;
 
         for y in 0..height {
             for x in 0..width {
-                let mut vec = Vec2::new((x as f32) / (width as f32), (y as f32) / (height as f32));
-                vec = vec * 2.0 - 1.0;
-                vec.x *= aspect_ratio;
+
+                let ray = self.camera.get_ray(x, y);
+                // let ray = Ray { origin: ray_origin, direction: ray_direction };
 
                 let color = convert_to_argb(
-                    &Self::per_pixel(vec)
-                        // &self.per_pixel(vec)
+                    &Self::trace_ray(ray)
                         .clamp(
                             Vec4::from((0.0, 0.0, 0.0, 0.0)),
-                            Vec4::from((1.0, 1.0, 1.0, 1.0)),
+                            Vec4::from((1.0, 1.0, 1.0, 1.0))
                         ),
                 );
 
@@ -72,23 +88,20 @@ impl RayTracer {
         self.last_render_time = render_start_time.elapsed();
     }
 
-    fn per_pixel(coord: Vec2) -> Vec4 /* returns color */ {
+    fn trace_ray(ray : Ray) -> Vec4 /* returns color */ {
         // (bx^2 + by^2 + bz^2)t^2 + 2(axbx + ayby + azbz)t + (ax^2 + ay^2 + az^2 - r^2)
         // a vec ray origin
         // b vec ray direction
         // r radius
         // t hit distance
 
-        // let ray_origin = Vec3::new(0.0, 2.0, 0.0);
-        let ray_origin = Vec3::new(0.0, 0.0, 2.0);
-        // let ray_direction = Vec3::new(coord.x, -1.0, coord.y);
-        let ray_direction = Vec3::new(coord.x, coord.y, -1.0);
+
         let radius = 1.0;
         let sphere_color = Vec3::new(1.0, 0.0, 1.0);
 
-        let a = ray_direction.dot(ray_direction);
-        let b = 2.0 * ray_direction.dot(ray_origin);
-        let c = ray_origin.dot(ray_origin) - radius * radius;
+        let a = ray.direction.dot(ray.direction);
+        let b = 2.0 * ray.direction.dot(ray.origin);
+        let c = ray.origin.dot(ray.origin) - radius * radius;
 
         let discriminant = b * b - 4.0 * a * c;
 
@@ -104,7 +117,7 @@ impl RayTracer {
         let t1 = (-b - sqrt_d) / (2.0 * a);
         let t = t0.min(t1);
 
-        let mut normal = ray_origin + t * ray_direction;
+        let mut normal = ray.origin + t * ray.direction;
 
         normal = normal.normalize();
 
